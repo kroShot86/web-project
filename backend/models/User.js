53cda99cc7aa056169acb8b2be2d47292476f29f
”kroShot” <kroShot86@mail.ru>
2026-01-31 01:02:32 +1000

    фикс багов 2.0

--- backend/models/User.js
+++ backend/models/User.js
@@ -36,3 +36,6 @@ const UserSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('User', UserSchema);
+
+
+
---
+++ text.py
@@ -0,0 +1,8 @@
+#Development режим
+#docker-compose -f docker-compose.yml -f docker-compose.dev.yml up --build
+
+# Production режим
+#docker-compose -f docker-compose.yml -f docker-compose.prod.yml up --build
+
+# Остановка контейнеров
+#docker-compose down
